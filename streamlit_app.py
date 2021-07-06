42868a47231c10424f3f41278670c1e019764e72
SylvainBallerini <ballerini.sylvain@gmail.com>
2021-07-07 00:19:50 +0200

    url csv pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Hello Wilders, Welcome to my application")
 
 st.write("I enjoy to discover streamlit possibilities")
 
-#link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
-#df_weather = pd.read_csv(link)
-#st.write(df_weather)
+link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
+df_weather = pd.read_csv(link)
+st.write(df_weather)
 
 name = st.text_input("Please give me your name : ")
 name_length = len(name)
