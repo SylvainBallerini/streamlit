411018c02a24ab19033c0e386e7a5aa47c043850
SylvainBallerini <ballerini.sylvain@gmail.com>
2021-07-07 00:25:13 +0200

    seaborn plotly no verion

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sns
+import plotly.figure_factory as ff
+import numpy as np
 
 st.title("Hello Wilders, Welcome to my application")
 
@@ -7,7 +10,7 @@ st.write("I enjoy to discover streamlit possibilities")
 
 link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
 df_weather = pd.read_csv(link)
-st.write(df_weather)
+#st.write(df_weather)
 
 name = st.text_input("Please give me your name : ")
 name_length = len(name)
@@ -21,3 +24,30 @@ else:
 agree = st.checkbox("I agree")
 if agree:
     st.write("Great!")
+
+df_weather
+
+st.line_chart(df_weather['MAX_TEMPERATURE_C'])
+
+viz_correlation = sns.heatmap(df_weather.corr(), 
+								center=0,
+								cmap = sns.color_palette("vlag", as_cmap=True)
+								)
+
+st.pyplot(viz_correlation.figure)
+
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
