4f7d03130d4ef89a7182886c577913ea7fe017ae
SylvainBallerini <ballerini.sylvain@gmail.com>
2021-07-07 00:16:39 +0200

    init 01

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
-st.title('Hello Wilders, welcome to my application!')
+st.title("Hello Wilders, Welcome to my application")
+
+st.write("I enjoy to discover streamlit possibilities")
+
+#link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
+#df_weather = pd.read_csv(link)
+#st.write(df_weather)
+
+name = st.text_input("Please give me your name : ")
+name_length = len(name)
+st.write("Your name has ", name_length,"characters")
+
+if st.button('Say hello'):
+     st.write('Why hello there')
+else:
+     st.write('Goodbye')
+
+agree = st.checkbox("I agree")
+if agree:
+    st.write("Great!")
